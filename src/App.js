36d75165e61a66f48578567313dd5fac9a3f4259
Ankit Kumar <myselfankit51@gmail.com>
2021-04-19 01:16:21 +0530

    again changed proxy url

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import {
   Container,
   CssBaseline,
   makeStyles,
-  ThemeProvider,
+  ThemeProvider
 } from "@material-ui/core";
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
@@ -20,7 +20,7 @@ import WatchList from "./pages/WatchList";
 import theme from "./theme";
 
 const client = new ApolloClient({
-  uri: "https://animeflix-server.herokuapp.com/",
+  uri: "https://animeflix-server.herokuapp.com/graphql",
   cache: new InMemoryCache(),
 });
 
